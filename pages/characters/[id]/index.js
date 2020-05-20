e8da4e375ee6128255ca758948b68869fbde7aeb
Jordi Dosne <jordi@dosne.net>
2020-05-20 17:00:31 +0200

    migrate to static props

--- pages/characters/[id]/index.js
+++ pages/characters/[id]/index.js
@@ -1,45 +1,65 @@
 import dynamic from "next/dynamic";
+import {useRouter} from 'next/router';
 
 import Head from "../../../components/Head";
 import Navigation from "../../../components/Navigation";
 import debug from "../../../helpers/debug";
 
 const MapNoSSR = dynamic(() => import("../../../components/Map"), {
-  ssr: false
+    ssr: false
 });
 
-export default function CharacterPage({ character }) {
-  return (
-    <div>
-      <Head>
-        <title>{character.name} presentation | Rick and Morty characters</title>
-      </Head>
+export default function CharacterPage({character}) {
+    const router = useRouter();
+    if (router.isFallback) {
+        return <div>
+            <title>Loading... | Rick and Morty characters</title>
+            <div>Loading...</div>
+        </div>;
+    }
 
-      <Navigation />
+    return (
+        <div>
+            <Head>
+                <title>{character.name} presentation | Rick and Morty characters</title>
+            </Head>
 
-      <h1>{character.name}</h1>
-      <img src={character.image} alt="" />
-      <div>
-        <li>species: {character.species}</li>
-        <li>gender: {character.gender}</li>
-        <li>origin: {character.origin.name}</li>
-      </div>
+            <Navigation/>
+            <h1>{character.name}</h1>
+            <img src={character.image} alt=""/>
+            <div>
+                <li>species: {character.species}</li>
+                <li>gender: {character.gender}</li>
+                <li>origin: {character.origin.name}</li>
+            </div>
 
-      <hr />
+            <hr/>
 
-      <MapNoSSR />
-    </div>
-  );
+            <MapNoSSR/>
+        </div>
+    );
 }
 
-CharacterPage.getInitialProps = async function(ctx) {
-  debug(ctx);
-  const id = ctx.query.id;
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character/${id}`
-  );
-  const character = await response.json();
-  return {
-    character
-  };
+export async function getStaticPaths() {
+    const response = await fetch(
+        `https://rickandmortyapi.com/api/character/`
+    );
+    const characters = await response.json();
+
+    return {
+        paths: characters.results.map(character => ({params: {id: String(character.id)}})),
+        fallback: true
+    };
+}
+
+export async function getStaticProps(ctx) {
+    debug(ctx);
+    const id = ctx.params.id;
+    const response = await fetch(
+        `https://rickandmortyapi.com/api/character/${id}`
+    );
+    const character = await response.json();
+    return {
+        props: {character},
+    };
 };
